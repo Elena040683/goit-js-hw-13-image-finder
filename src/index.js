249987687c85cc154abd0228b0c78fe5a879d982
Elena <Elena3405674@mail.ru>
2021-09-04 04:36:04 +0300

    Gallary

--- src/index.js
+++ src/index.js
@@ -1,27 +1,87 @@
 import ImgTpl from './templates/img.hbs';
-import fetchImages from './js/apiService';
+import ApiService from './js/apiService';
+import LoadMoreBtn from './js/loadMore';
+
+import { inform, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = {
   form: document.querySelector('#search-form'),
-  input: document.querySelector('#search'),
   container: document.querySelector('.gallery'),
-  more: document.querySelector('#more'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
-function renderImages(data) {
-  const markup = ImgTpl(data);
-  refs.container.innerHTML = markup;
-}
+const newApiService = new ApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
 
 refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.addEventListener('click', handleBtnClick);
 
 function onSearch(e) {
   e.preventDefault();
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
 
-  fetchImages(searchQuery)
-    .then(renderImages)
-    .catch(err => console.log(err))
-    .finally(() => form.reset());
+  newApiService.query = e.currentTarget.elements.query.value;
+
+  if (newApiService.query === '') {
+    return inform({
+      text: 'Enter the value!',
+      delay: 1500,
+      closerHover: true,
+    });
+  }
+
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  newApiService.resetPage();
+  newApiService.fetchImages().then(data => {
+    clearImagesContainer();
+    renderImages(data);
+    loadMoreBtn.enable();
+  });
 }
+
+function renderImages(data) {
+  refs.container.insertAdjacentHTML('beforeend', ImgTpl(data));
+}
+
+function clearImagesContainer() {
+  refs.container.innerHTML = '';
+}
+
+function onLoadMore() {
+  loadMoreBtn.disable();
+
+  newApiService.fetchImages().then(data => {
+    renderImages(data);
+    loadMoreBtn.enable();
+  });
+}
+
+let element = document.getElementById('img');
+
+function handleBtnClick() {
+  setTimeout(() => {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  }, 700);
+}
+
+// function onLoadMore() {
+//   fetchImages()
+//     .then(
+//       setTimeout(() => {
+//         window.scrollBy({
+//           top: document.documentElement.clientHeight - 100,
+//           behavior: 'smooth',
+//         });
+//       }, 1500),
+//     )
+//     .catch(err => console.log(err));
+// }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,30 @@
-function fetchImages(searchQuery) {
-  return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=23140878-cc837524a3fe3a996e92890e9`,
-  ).then(res => res.json());
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchImages() {
+    const API_KEY = '23140878-cc837524a3fe3a996e92890e9';
+    const BASE_URL = 'https://pixabay.com/api/';
+    return fetch(
+      `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    )
+      .then(res => res.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
